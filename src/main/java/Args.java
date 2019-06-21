95406a3265b6b62db20ba1e8f7ab43fb54245aa3
Sina Kashi Pazha <esterlinkof@gmail.com>
2019-06-22 01:32:25 +0430

    Refactor key generation and program arguments.

--- src/main/java/Args.java
+++ src/main/java/Args.java
@@ -4,18 +4,28 @@ import lombok.ToString;
 @ToString
 public class Args {
 
-    @Parameter(names ="-pr", description = "Private Key output file name path")
+    @Parameter(names ="--pr", description = "Private Key output file name path")
     public String prPath = "PRIVATE.key";
 
-    @Parameter(names = "-pub", description = "Public Key output file name path")
+    @Parameter(names = "--pub", description = "Public Key output file name path")
     public String pubPath = "PUBLIC.key";
 
     @Parameter(names = {"-i", "--input"} , description = "Input file name path")
     public String inputFilePath = "input.txt";
 
     @Parameter(names = {"-o", "--output"} , description = "Output file name path")
-    public String outputFilePath = "input.txt.enc";
+    public String outputFilePath = "output.txt";
 
-    @Parameter(names = "-debug", description = "Debug mode")
+    @Parameter(names = "--gen", description = "Generate keys")
+    public boolean gen = false;
+
+    @Parameter(names = "--enc", description = "Encryption mode")
+    public boolean encrypt = false;
+
+    @Parameter(names = "--dec", description = "Encryption mode")
+    public boolean decrypt = false;
+
+    @Parameter(names = "--debug", description = "Debug mode")
     public boolean debug = false;
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,6 +22,25 @@ public class Main {
         return args;
     }
 
+    private static void generateKeys(Args args) throws IOException {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Please input two prime numbers:");
+
+        PrivateKey pr = RSA.generatePrivateKey(console.nextInt(), console.nextInt());
+        PublicKey pub = RSA.exctractPublicKey(pr);
+
+        System.out.println(pr + "\n" + pub);
+
+        @Cleanup FileWriter pubFile = new FileWriter(args.pubPath),
+                prFile = new FileWriter(args.prPath);
+
+        prFile.write(pr.getD() + "\n" + pr.getN());
+        pubFile.write(pub.getE() + "\n" + pub.getN());
+
+        prFile.flush();
+        pubFile.flush();
+    }
+
     public static void main(String[] argv) {
 
         Args args = parsArgs(argv);
@@ -29,42 +48,33 @@ public class Main {
         if (args.debug) // todo clear this shit
             System.out.println(args);
 
-        Scanner console = new Scanner(System.in);
-        System.out.println("Please input two prime numbers:");
         try {
-            PrivateKey pr = RSA.generatePrivateKey(console.nextInt(), console.nextInt());
-            PublicKey pub = RSA.exctractPublicKey(pr);
+            if (args.gen)
+                generateKeys(args);
+            else if (args.encrypt) {
 
-            System.out.println(pr);
-            System.out.println(pub);
+                // todo read keys
 
-            @Cleanup FileWriter pubFile = new FileWriter(args.pubPath),
-                    prFile = new FileWriter(args.prPath),
-                    outputFile = new FileWriter(args.outputFilePath);
+                @Cleanup FileWriter outputFile = new FileWriter(args.outputFilePath);
+                @Cleanup FileReader inputFile = new FileReader(args.inputFilePath);
 
-            @Cleanup FileReader inputFile = new FileReader(args.inputFilePath);
+                int tmp;
+                do {
+                    tmp = inputFile.read();
+                    //tmp = RSA.encrypt(pub, tmp);
+                    outputFile.write(tmp);
+                } while (tmp != -1);
 
-            prFile.write(pr.getD() + "\n");
-            prFile.write(pr.getN() + "");
+                outputFile.flush();
 
-            pubFile.write(pub.getE() + "\n");
-            pubFile.write(pub.getN() + "");
-
-
-            int tmp;
-            do {
-                tmp = inputFile.read();
-                tmp = RSA.encrypt(pub, tmp);
-                outputFile.write(tmp);
-            } while (tmp != -1);
-
-            outputFile.flush();
-
+            } else if (args.decrypt) {
+                // todo read keys
+                // todo decrypt
+            }
         } catch (RuntimeException e) {
             System.out.println(e.getMessage());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
