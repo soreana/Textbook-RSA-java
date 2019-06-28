3351ecb29b0c02e68d51eb9ad210856facb1f8a3
Sina Kashi Pazha <esterlinkof@gmail.com>
2019-06-28 17:02:18 +0430

    Finalize private key java doc.

--- src/main/java/rsa/PrivateKey.java
+++ src/main/java/rsa/PrivateKey.java
@@ -1,15 +1,39 @@
 package rsa;
 
+/**
+ * Interface used to represent Private Key. Every private key implementation should implement this interface.
+ *
+ * @author Sina Kashipazha
+ */
+
 public interface PrivateKey {
 
+    /**
+     * @return n , n used to calculate encrypted message in C = M^e mod n formula
+     */
+
     int getN();
+
+    /**
+     * @return e , e used to calculate decrypted message in M = C^d mod n formula
+     **/
+
     int getE();
+
+    /**
+     * @return d , d used to calculate decrypted message in M = C^d mod n formula
+     **/
+
     int getD();
+
+    /**
+     * @return BLOCK_SIZE , Cyphered message store in BLOCK_SIZE * Byte storage
+     */
+
     int getBlockSize();
 
     /**
-     *
-     * @return true if private key can used to generate public key
+     * @return true if private key contain public key parameters
      */
 
     boolean hasPublicKeyParameters();
--- src/main/java/rsa/PrivateKeyImp.java
+++ src/main/java/rsa/PrivateKeyImp.java
@@ -2,20 +2,41 @@ package rsa;
 
 import lombok.Getter;
 
+/**
+ * This class implement PrivateKey interface in simplest possible way.
+ *
+ * @author Sina Kashipazha
+ */
+
 public class PrivateKeyImp implements PrivateKey {
+
+    /**
+     * d used to calculate decrypted message in M = C^d mod n formula
+     * n used to calculate encrypted message C = M^e mod n
+     * e used to calculate encrypted message C = M^e mod n
+     * BLOCK_SIZE , Cyphered message store in BLOCK_SIZE * Byte storage
+     */
 
     @Getter
     private final int d, n, e;
     private final int BLOCK_SIZE;
 
-    public PrivateKeyImp(int p, int q) {
+    /**
+     * Calculate new private key parameters using prime numbers p and q.
+     */
+
+    PrivateKeyImp(int p, int q) {
         this.e = RSA.calculateE(p, q);
         this.d = RSA.calculateD(RSA.calculatePhiN(p,q), this.e);
         this.n = p * q;
         this.BLOCK_SIZE = RSA.blockSize(this.n);
     }
 
-    public PrivateKeyImp(int d, int n, int e){
+    /**
+     * Store previously calculated private key parameters to new Object.
+     */
+
+    PrivateKeyImp(int d, int n, int e){
         this.d = d;
         this.n = n;
         this.e = e;
@@ -27,10 +48,18 @@ public class PrivateKeyImp implements PrivateKey {
         return String.format("Private key parameters are:\nd: %d, n: %d, e: %d\nBlock size is %d", d, n, e, BLOCK_SIZE);
     }
 
+    /**
+     * @return BLOCK_SIZE , Cyphered message store in BLOCK_SIZE * Byte storage
+     */
+
     @Override
     public int getBlockSize() {
         return BLOCK_SIZE;
     }
+
+    /**
+     * @return true if private key contain public key parameters
+     */
 
     @Override
     public boolean hasPublicKeyParameters() {
