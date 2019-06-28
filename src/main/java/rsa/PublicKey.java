d5ec4429bc8efbcd218deb1f941d61b321fbe22e
Sina Kashi Pazha <esterlinkof@gmail.com>
2019-06-28 16:24:52 +0430

    Finalize public key java doc.

--- src/main/java/rsa/PublicKey.java
+++ src/main/java/rsa/PublicKey.java
@@ -1,9 +1,28 @@
 package rsa;
 
+/**
+ * Interface used to represent Public Key. Every public key implementation should implement this interface.
+ *
+ * @author Sina Kashipazha
+ */
 
 public interface PublicKey {
 
+    /**
+     * @return e , e used to calculate encrypted message in C = M^e mod n formula
+     **/
+
     int getE();
+
+    /**
+     * @return n , n used to calculate encrypted message in C = M^e mod n formula
+     */
+
     int getN();
+
+    /**
+     * @return BLOCK_SIZE , Cyphered message store in BLOCK_SIZE * Byte storage
+     */
+
     int getBlockSize();
 }
--- src/main/java/rsa/PublicKeyImp.java
+++ src/main/java/rsa/PublicKeyImp.java
@@ -2,25 +2,57 @@ package rsa;
 
 import lombok.Getter;
 
+/**
+ * This class implement PublicKey interface in simplest possible way.
+ *
+ * @author Sina Kashipazha
+ */
+
 public class PublicKeyImp implements PublicKey {
+
+    /**
+     * e used to calculate encrypted message C = M^e mod n
+     * n used to calculate encrypted message C = M^e mod n
+     * BLOCK_SIZE , Cyphered message store in BLOCK_SIZE * Byte storage
+     */
+
     @Getter
     private final int e, n;
     private final int BLOCK_SIZE;
 
-    public PublicKeyImp(int e, int n, int BLOCK_SIZE) {
+    /**
+     * Create new public key Object with respect to e and n. Nothing checks.
+     *
+     * @param e          e used to calculate encrypted message C = M^e mod n
+     * @param n          n used to calculate encrypted message C = M^e mod n
+     * @param BLOCK_SIZE , Cyphered message store in BLOCK_SIZE * Byte storage
+     */
+
+    PublicKeyImp(int e, int n, int BLOCK_SIZE) {
         this.e = e;
         this.n = n;
         this.BLOCK_SIZE = BLOCK_SIZE;
     }
 
-    public PublicKeyImp(int e, int n){
-        this(e,n,RSA.blockSize(n));
+    /**
+     * Create new public key Object with respect to e and n. Nothing checks.
+     *
+     * @param e e used to calculate encrypted message C = M^e mod n
+     * @param n n used to calculate encrypted message C = M^e mod n
+     */
+
+    PublicKeyImp(int e, int n) {
+        this(e, n, RSA.blockSize(n));
     }
 
     @Override
     public String toString() {
         return String.format("Public key parameters are:\ne: %d, n: %d\nBlock size is %d", e, n, BLOCK_SIZE);
     }
+
+    /**
+     * @return BLOCK_SIZE , Cyphered message store in BLOCK_SIZE * Byte storage
+     */
 
     @Override
     public int getBlockSize() {
